7a3c24ff3c10be3e4fa0e6c857f9b2d5a19a8e52
osmancansiz <osmancansiz@github.com>
2020-02-20 01:14:46 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ionic-take-photo
---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from "@angular/core";
+
+import {
+  Plugins,
+  CameraResultType,
+  Capacitor,
+  FilesystemDirectory,
+  CameraPhoto,
+  CameraSource
+} from "@capacitor/core";
+
+const { Camera, Filesystem, Storage } = Plugins;
+
+@Injectable({
+  providedIn: "root"
+})
+export class PhotoService {
+  public photos: Photo[] = [];
+
+  constructor() {}
+
+  public async addNewToGallery() {
+    // Take a photo
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100
+    });
+
+    this.photos.unshift({
+      filepath: "soon...",
+      webviewPath: capturedPhoto.webPath
+    });
+  }
+}
+
+interface Photo {
+  filepath: string;
+  webviewPath: string;
+  base64?: string;
+}
